e9256e4ed860ad1c535c40e9ae8e9415b31ca750
zhengkang86 <zhengkang86@gmail.com>
2019-07-15 12:18:01 -0400

    Update install_ubuntu_essentials.sh

--- install_ubuntu_essentials.sh
+++ install_ubuntu_essentials.sh
@@ -1,5 +1,5 @@
 # System essentials
-sudo apt install htop lm-sensors tmux screen vim git
+sudo apt install htop lm-sensors tmux screen vim git -y
 
 # Other packages
-sudo apt install inkscape gimp
+sudo apt install inkscape gimp -y
