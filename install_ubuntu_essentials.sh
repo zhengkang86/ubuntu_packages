b25fb1c7d8b76ae0a5355c8ac39f873d22195d0d
Kang Zheng <zhengkang86@gmail.com>
2019-07-15 17:32:09 -0400

    Added installation for sublime text and mathpix snipping tool.

--- install_ubuntu_essentials.sh
+++ install_ubuntu_essentials.sh
@@ -3,3 +3,13 @@ sudo apt install htop lm-sensors tmux screen vim git -y
 
 # Other packages
 sudo apt install inkscape gimp -y
+
+# Sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text -y
+
+# Mathpix Snipping tool
+sudo snap install mathpix-snipping-tool
