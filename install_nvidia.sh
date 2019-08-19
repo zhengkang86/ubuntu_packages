692e1a2882eb4c7ee564c336b7c8c0fd6091a648
zhengkang86 <zhengkang86@gmail.com>
2019-08-19 15:45:15 -0400

    Update install_nvidia.sh

--- install_nvidia.sh
+++ install_nvidia.sh
@@ -4,6 +4,9 @@
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt update
 
+# after adding the graphics driver repository, use the Software & Updates tool to select nvidia-390 to install. 
+# then, install newer versions.
+
 # Install nvidia-modprobe, which is crucial
 sudo apt install nvidia-driver-430 nvidia-modprobe
 sudo reboot
